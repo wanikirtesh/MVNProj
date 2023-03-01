3af3c52e533805172bc714928551cc64e27c11d9
Kirtesh Wani <wanikirtesh@gmail.com>
2023-03-01 08:47:44 +0530

    adding Data provider to run the test for multiple data sets

--- src/test/java/com/sbn/tests/LoginTest.java
+++ src/test/java/com/sbn/tests/LoginTest.java
@@ -1,13 +1,15 @@
 package com.sbn.tests;
-
 import com.sbn.pages.LoginPage;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.*;
-import util.TestConfigReader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginTest extends BaseTest{
-
     @Test
     @Parameters({"user","password"})
     public void validateValidLoginCredentials(String user, String password){
@@ -16,10 +18,28 @@ public class LoginTest extends BaseTest{
         Assert.assertTrue(isLoggedIn);
     }
 
-    @Test
-    public void validateInvalidLoginCredentials(){
+    @Test(dataProvider = "simpleInvalidCred")
+    public void validateInvalidLoginCredentials(String user, String password){
         LoginPage loginPage = new LoginPage(driver);
-        boolean isLoggedIn = loginPage.doLogin("user","password");
+        boolean isLoggedIn = loginPage.doLogin(user,password);
         Assert.assertFalse(isLoggedIn);
+    }
+
+
+    @DataProvider(name = "fileBasedInvalidCred")
+    Object[][] myData() throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("./inputData/invalidCred.csv"));
+        String[][] invalidCred = new String[lines.size()][2];
+        int i=0;
+        for (String line : lines) {
+            invalidCred[i++] = line.split(",");
+        }
+        return invalidCred;
+    }
+
+    @DataProvider(name = "simpleInvalidCred")
+    Object[][] invalidCred(){
+        String[][] data = {{"user1","password1"},{"user2","password2"}};
+        return data;
     }
 }
